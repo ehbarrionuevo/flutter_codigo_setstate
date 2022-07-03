524f07d007e6853abb64671120301176c386a904
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-03 12:11:21 -0500

    Mods

--- lib/pages/status_general_page.dart
+++ lib/pages/status_general_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class StatusGeneralPage extends StatelessWidget {
-  const StatusGeneralPage({Key? key}) : super(key: key);
+class StatusGeneralPage extends StatefulWidget {
+  @override
+  State<StatusGeneralPage> createState() => _StatusGeneralPageState();
+}
+
+class _StatusGeneralPageState extends State<StatusGeneralPage> {
+  bool isFontWeight = false;
+  bool isTextDecoration = false;
 
   @override
   Widget build(BuildContext context) {
@@ -32,16 +38,58 @@ class StatusGeneralPage extends StatelessWidget {
               ),
             ),
             const SizedBox(
-              height: 12.0,
+              height: 14.0,
             ),
             Text(
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: isFontWeight ? FontWeight.bold : FontWeight.normal,
+                decoration: isTextDecoration
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
             ),
             const SizedBox(
-              height: 12.0,
+              height: 8.0,
+            ),
+            const Divider(),
+            const SizedBox(
+              height: 8.0,
             ),
             Text(
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+            ),
+            const Divider(),
+            const SizedBox(
+              height: 14.0,
+            ),
+            // Checkbox(
+            //   value: true,
+            //   onChanged: (bool? value) {
+            //     print(value);
+            //   },
+            // ),
+            CheckboxListTile(
+              value: isFontWeight,
+              onChanged: (bool? value) {
+                isFontWeight = value!;
+                setState(() {});
+              },
+              title: Text(
+                "Cambiar Font Weight:: $isFontWeight",
+              ),
+            ),
+            CheckboxListTile(
+              value: isTextDecoration,
+              onChanged: (bool? value) {
+                isTextDecoration = value!;
+                setState(() {});
+              },
+              title: Text(
+                "Cambiar TextDecoration:: $isFontWeight",
+              ),
             ),
           ],
         ),
